a326da0691a6b7c23723359c7a295806bdceefd0
Nikita Kuryavtsev <Nikita.Kudryavcev@innostage-group.ru>
2025-03-05 22:01:27 +0300

    add tooltips ui components

---
+++ src/components/ui/tooltip/AppTooltip.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+
+const props = defineProps<{ text: string }>();
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <slot name="trigger" />
+      </TooltipTrigger>
+      <TooltipContent>
+        <p class="text-xl">{{ text }}</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
